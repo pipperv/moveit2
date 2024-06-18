6bd1bb9ac6d9a85086a4b8bdcccd72af4c552b12
Felipe Valenzuela <felipeandres.valenzuelar@gmail.com>
2024-06-18 15:17:43 -0400

    ur servo

--- moveit_ros/moveit_servo/src/teleop_demo/joystick_servo_example.cpp
+++ moveit_ros/moveit_servo/src/teleop_demo/joystick_servo_example.cpp
@@ -58,40 +58,41 @@
 const std::string JOY_TOPIC = "/joy";
 const std::string TWIST_TOPIC = "/servo_node/delta_twist_cmds";
 const std::string JOINT_TOPIC = "/servo_node/delta_joint_cmds";
-const std::string EEF_FRAME_ID = "panda_hand";
-const std::string BASE_FRAME_ID = "panda_link0";
+const std::string EEF_FRAME_ID = "tool0";
+const std::string BASE_FRAME_ID = "base_link";
 
 // Enums for button names -> axis/button array index
-// For XBOX 1 controller
+// For Nintendo Switch controller
 enum Axis
 {
   LEFT_STICK_X = 0,
   LEFT_STICK_Y = 1,
-  LEFT_TRIGGER = 2,
-  RIGHT_STICK_X = 3,
-  RIGHT_STICK_Y = 4,
-  RIGHT_TRIGGER = 5,
-  D_PAD_X = 6,
-  D_PAD_Y = 7
+  RIGHT_STICK_X = 2,
+  RIGHT_STICK_Y = 3,
+  D_PAD_X = 4,
+  D_PAD_Y = 5
 };
 enum Button
 {
-  A = 0,
-  B = 1,
+  B = 0,
+  A = 1,
   X = 2,
   Y = 3,
-  LEFT_BUMPER = 4,
-  RIGHT_BUMPER = 5,
-  CHANGE_VIEW = 6,
-  MENU = 7,
-  HOME = 8,
-  LEFT_STICK_CLICK = 9,
-  RIGHT_STICK_CLICK = 10
+  SCREENSHOT = 4,
+  LEFT_BUMPER = 5,
+  RIGHT_BUMPER = 6,
+  LEFT_TRIGGER = 7,
+  RIGHT_TRIGGER = 8,
+  MINUS = 9,
+  PLUS = 10,
+  HOME = 11,
+  LEFT_STICK_CLICK = 12,
+  RIGHT_STICK_CLICK = 13
 };
 
 // Some axes have offsets (e.g. the default trigger position is 1.0 not 0)
 // This will map the default values for the axes
-std::map<Axis, double> AXIS_DEFAULTS = { { LEFT_TRIGGER, 1.0 }, { RIGHT_TRIGGER, 1.0 } };
+// std::map<Axis, double> AXIS_DEFAULTS = { { LEFT_TRIGGER, 1.0 }, { RIGHT_TRIGGER, 1.0 } };
 std::map<Button, double> BUTTON_DEFAULTS;
 
 // To change controls or setup a new controller, all you should to do is change the above enums and the follow 2
@@ -109,36 +110,39 @@ bool convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& but
 {
   // Give joint jogging priority because it is only buttons
   // If any joint jog command is requested, we are only publishing joint commands
-  if (buttons[A] || buttons[B] || buttons[X] || buttons[Y] || axes[D_PAD_X] || axes[D_PAD_Y])
-  {
-    // Map the D_PAD to the proximal joints
-    joint->joint_names.push_back("panda_joint1");
-    joint->velocities.push_back(axes[D_PAD_X]);
-    joint->joint_names.push_back("panda_joint2");
-    joint->velocities.push_back(axes[D_PAD_Y]);
+  // if (buttons[A] || buttons[B] || buttons[X] || buttons[Y] || axes[D_PAD_X] || axes[D_PAD_Y])
+  // {
+  //   // Map the D_PAD to the proximal joints
+  //   joint->joint_names.push_back("panda_joint1");
+  //   joint->velocities.push_back(axes[D_PAD_X]);
+  //   joint->joint_names.push_back("panda_joint2");
+  //   joint->velocities.push_back(axes[D_PAD_Y]);
 
-    // Map the diamond to the distal joints
-    joint->joint_names.push_back("panda_joint7");
-    joint->velocities.push_back(buttons[B] - buttons[X]);
-    joint->joint_names.push_back("panda_joint6");
-    joint->velocities.push_back(buttons[Y] - buttons[A]);
-    return false;
-  }
+  //   // Map the diamond to the distal joints
+  //   joint->joint_names.push_back("panda_joint7");
+  //   joint->velocities.push_back(buttons[B] - buttons[X]);
+  //   joint->joint_names.push_back("panda_joint6");
+  //   joint->velocities.push_back(buttons[Y] - buttons[A]);
+  //   return false;
+  // }
 
   // The bread and butter: map buttons to twist commands
-  twist->twist.linear.z = axes[RIGHT_STICK_Y];
-  twist->twist.linear.y = axes[RIGHT_STICK_X];
+  twist->twist.linear.x = axes[LEFT_STICK_Y];
+  twist->twist.linear.y = axes[LEFT_STICK_X];
+  twist->twist.linear.z = axes[D_PAD_Y];
 
-  double lin_x_right = -0.5 * (axes[RIGHT_TRIGGER] - AXIS_DEFAULTS.at(RIGHT_TRIGGER));
-  double lin_x_left = 0.5 * (axes[LEFT_TRIGGER] - AXIS_DEFAULTS.at(LEFT_TRIGGER));
-  twist->twist.linear.x = lin_x_right + lin_x_left;
 
-  twist->twist.angular.y = axes[LEFT_STICK_Y];
-  twist->twist.angular.x = axes[LEFT_STICK_X];
+  // double lin_x_right = -0.5 * (axes[RIGHT_TRIGGER] - AXIS_DEFAULTS.at(RIGHT_TRIGGER));
+  // double lin_x_left = 0.5 * (axes[LEFT_TRIGGER] - AXIS_DEFAULTS.at(LEFT_TRIGGER));
+  // twist->twist.linear.x = lin_x_right + lin_x_left;
 
-  double roll_positive = buttons[RIGHT_BUMPER];
-  double roll_negative = -1 * (buttons[LEFT_BUMPER]);
-  twist->twist.angular.z = roll_positive + roll_negative;
+  twist->twist.angular.x = -axes[RIGHT_STICK_X];
+  twist->twist.angular.y = axes[RIGHT_STICK_Y];
+  twist->twist.angular.z = -axes[D_PAD_X];
+
+  // double roll_positive = buttons[RIGHT_BUMPER];
+  // double roll_negative = -1 * (buttons[LEFT_BUMPER]);
+  // twist->twist.angular.z = roll_positive + roll_negative;
 
   return true;
 }
@@ -149,9 +153,9 @@ bool convertJoyToCmd(const std::vector<float>& axes, const std::vector<int>& but
  */
 void updateCmdFrame(std::string& frame_name, const std::vector<int>& buttons)
 {
-  if (buttons[CHANGE_VIEW] && frame_name == EEF_FRAME_ID)
+  if (buttons[MINUS] && frame_name == EEF_FRAME_ID)
     frame_name = BASE_FRAME_ID;
-  else if (buttons[MENU] && frame_name == BASE_FRAME_ID)
+  else if (buttons[PLUS] && frame_name == BASE_FRAME_ID)
     frame_name = EEF_FRAME_ID;
 }
 
@@ -181,37 +185,8 @@ public:
     // Load the collision scene asynchronously
     collision_pub_thread_ = std::thread([this]() {
       rclcpp::sleep_for(std::chrono::seconds(3));
-      // Create collision object, in the way of servoing
-      moveit_msgs::msg::CollisionObject collision_object;
-      collision_object.header.frame_id = "panda_link0";
-      collision_object.id = "box";
-
-      shape_msgs::msg::SolidPrimitive table_1;
-      table_1.type = table_1.BOX;
-      table_1.dimensions = { 0.4, 0.6, 0.03 };
-
-      geometry_msgs::msg::Pose table_1_pose;
-      table_1_pose.position.x = 0.6;
-      table_1_pose.position.y = 0.0;
-      table_1_pose.position.z = 0.4;
-
-      shape_msgs::msg::SolidPrimitive table_2;
-      table_2.type = table_2.BOX;
-      table_2.dimensions = { 0.6, 0.4, 0.03 };
-
-      geometry_msgs::msg::Pose table_2_pose;
-      table_2_pose.position.x = 0.0;
-      table_2_pose.position.y = 0.5;
-      table_2_pose.position.z = 0.25;
-
-      collision_object.primitives.push_back(table_1);
-      collision_object.primitive_poses.push_back(table_1_pose);
-      collision_object.primitives.push_back(table_2);
-      collision_object.primitive_poses.push_back(table_2_pose);
-      collision_object.operation = collision_object.ADD;
 
       moveit_msgs::msg::PlanningSceneWorld psw;
-      psw.collision_objects.push_back(collision_object);
 
       auto ps = std::make_unique<moveit_msgs::msg::PlanningScene>();
       ps->world = psw;
